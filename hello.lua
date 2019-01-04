c2f3967f0505b2fe8f58371946e564be9ac187c5
xuxq1992 <30610174+xuxq1992@users.noreply.github.com>
2019-01-04 13:52:20 +0800

    Create hello.lua

---
+++ hello.lua
@@ -0,0 +1,10 @@
+str = "I am so cool"  
+tbl = {name = "shun", id = 20114442}  
+function add(a,b)  
+    return a * b  
+end
+local function init()
+    print("正在加载脚本。。。。。。。")
+    dofile("operate.lua")
+end
+init()
